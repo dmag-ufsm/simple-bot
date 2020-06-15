986f81bc185f154f3674196f1b6a45518965f5eb
ggpereira <ggomesper@gmail.com>
2020-06-15 15:53:14 -0300

    bugfix: observer being triggered more than once

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import json
 import time
-from watchdog.observers import Observer
+
+# from watchdog.observers import Observer
+# BugFix use this if the same event is being fired multiple times when using Observer
+from watchdog.observers.polling import PollingObserver as Observer
 from watchdog.events import PatternMatchingEventHandler
 import sys
+
 
 class JSONHandler(PatternMatchingEventHandler):
     patterns = ["*/game_status.json"]
@@ -10,6 +14,9 @@ class JSONHandler(PatternMatchingEventHandler):
     @staticmethod
     def process(event):
         print(event.src_path, event.event_type)
+        game_state, player_state = read_json('./game_info/game_status.json', '0')
+        print(game_state)
+        print(player_state)
 
     def on_modified(self, event):
         self.process(event)
@@ -29,14 +36,14 @@ def read_json(path, player_id):
     return game_status, player_status
 
 
-## game_state, player_state = read_json('./game_info/game_status.json', '0')
-
 if __name__ == '__main__':
     args = sys.argv[1:]
     observer = Observer()
+    observer.event_queue.maxsize = 0
     observer.schedule(JSONHandler(), path=args[0] if args else '.')
     observer.start()
 
+    # watch for changes in game_status.json
     print('Watching {} ...'.format(args[0]))
 
     try:
