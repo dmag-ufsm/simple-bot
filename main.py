a6b7f89e72333b47fc991df330784e263554cb1c
ggpereira <ggomesper@gmail.com>
2020-06-16 00:06:01 -0300

    read and transform data from csv

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import time
+import pandas
+import math
 
 # from watchdog.observers import Observer
 # BugFix use this if the same event is being fired multiple times when using Observer
@@ -8,8 +10,12 @@ from watchdog.events import PatternMatchingEventHandler
 import sys
 
 
-class JSONHandler(PatternMatchingEventHandler):
+class BotInputHandler(PatternMatchingEventHandler):
     patterns = ["*/game_status.json"]
+
+    def __init__(self, cards, weights):
+        self.cards_data = cards
+        self.weights_data = weights
 
     # takes action when game_status.json updated or created
     @staticmethod
@@ -37,11 +43,39 @@ def read_json(path, player_id):
     return game_status, player_status
 
 
+# remove card names underline
+def transform_card_names(data):
+    cards = data.loc[0: 77, 'card_name']
+    for index in range(77):
+        cards.loc[index] = cards.loc[index].replace('_', ' ')
+
+
+# transformation by removing '*' and replacing nan with 1
+def transform_cards_weight(data):
+    for index in range(1, 79):
+        weight = data['Military Strategy'][index]
+
+        if isinstance(weight, str):
+            weight = weight.replace('*', '')
+
+        elif math.isnan(weight):
+            weight = 1
+
+        data['Military Strategy'][index] = weight
+
+
 if __name__ == '__main__':
+
+    cards_data = pandas.read_csv('./data/cards.csv')
+    weight_cards_data = pandas.read_csv('./data/weightPerCard.csv')
+
+    transform_card_names(cards_data)
+    transform_cards_weight(weight_cards_data)
+
     args = sys.argv[1:]
     observer = Observer()
     observer.event_queue.maxsize = 0
-    observer.schedule(JSONHandler(), path=args[0] if args else '.')
+    observer.schedule(BotInputHandler(cards_data, weight_cards_data), path=args[0] if args else '.')
     observer.start()
 
     # watch for changes in game_status.json
@@ -54,6 +88,3 @@ if __name__ == '__main__':
         observer.stop()
 
     observer.join()
-
-
-
