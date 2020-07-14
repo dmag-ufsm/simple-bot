305b82f768e4aae7d8d508ded8347e0ad99c1be1
gabriel <ggomesper@gmail.com>
2020-07-14 18:05:38 -0300

    match_monitor.py updated

--- match_monitor.py
+++ match_monitor.py
@@ -12,7 +12,7 @@ from watchdog.events import PatternMatchingEventHandler
 from datetime import datetime
 import sys
 
-class BotInputHandler(PatternMatchingEventHandler):
+class MatchLogHandler(PatternMatchingEventHandler):
     patterns = [sys.argv[1] + '/game_status.json']
 
     def __init__(self):
@@ -22,11 +22,12 @@ class BotInputHandler(PatternMatchingEventHandler):
     def process(self, event):
         print(event.src_path, event.event_type)
         # game_state, players_state = read_json('../Game/io/game_status.json')
-        data = read_json('game_info/game_status.json')
-
+        # data = read_json('../Game/build/io/game_status.json')
+        data = read_json('../Game/io/game_status.json')
+    
         # game end
         if (data['game']['era'] == 3) & (data['game']['turn'] == 20):
-            file_name = 'match_{}.json'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S"))
+            file_name = 'match_test-{}.json'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S"))
             with open('match_logs/match/{}'.format(file_name), 'w') as fp:
                 print('saving match data...')
                 json.dump(data, fp)
@@ -55,7 +56,7 @@ if __name__ == '__main__':
 
     observer = Observer()
     observer.event_queue.maxsize = 0
-    observer.schedule(BotInputHandler(), path=args[0] if args else '.')
+    observer.schedule(MatchLogHandler(), path=args[0] if args else '.')
     observer.start()
 
     # watch for changes in game_status.json
